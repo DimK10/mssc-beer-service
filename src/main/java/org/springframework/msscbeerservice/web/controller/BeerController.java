40282b5da9b8dd0631fb3edfe3fc54d512114391
dkaitan <dkaitan@mobics.gr>
2023-04-27 08:42:48 +0300

    Lesson 48 finished

--- src/main/java/org/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/org/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,11 +1,18 @@
 package org.springframework.msscbeerservice.web.controller;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.msscbeerservice.web.model.BeerDto;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/beer")
 @RestController
@@ -22,7 +29,10 @@ public class BeerController {
     public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
 
         // todo impl
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/");
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
