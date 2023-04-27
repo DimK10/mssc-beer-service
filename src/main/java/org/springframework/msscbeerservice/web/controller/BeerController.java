11e7752480b9e7185d48f24604697f80a06e6936
dkaitan <dkaitan@mobics.gr>
2023-04-27 12:06:51 +0300

    Adds delete mapping - Lesson 50 complete

--- src/main/java/org/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/org/springframework/msscbeerservice/web/controller/BeerController.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/beer")
@@ -40,5 +42,12 @@ public class BeerController {
 
         // todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{beerId}"})
+    public ResponseEntity deleteBeer(@PathVariable("beerId") UUID beerId){
+        
+        //todo impl 
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
